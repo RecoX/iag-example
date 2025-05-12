49585b578ea32cdce603b911661f6d4e893c75f7
Lucas Recoaro <lucas.recoaro@gmail.com>
2025-05-12 14:25:56 +1200

    Configured tailwind installation to work with build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 /// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
